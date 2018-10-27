ab45236bbed131d64a2c19d0355281a8d7e32466
push24 <push.ks24@gmail.com>
2018-10-27 18:19:27 +0530

    Initial commit

--- sw.js
+++ sw.js
@@ -5,7 +5,8 @@ self.addEventListener('install', event => {
     event.waitUntil(
       caches.open('v1').then(cache => {
         return cache.addAll([
-          '/'
+          '/',
+          'https://apis.google.com/js/platform.js'
 
         ]);
       })
